5b049b803586d7c21ef404f1d06389cc6c6b0538
Andrii Semenets <semteacher@gmail.com>
2026-08-17 15:58:25 +0300

    fix generator to use proper API

--- tests/generator/lib.php
+++ tests/generator/lib.php
@@ -15,6 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 use local_entities\settings_manager;
+use local_entities\form\edit_dynamic_form;
 
 /**
  * Class local_entities_generator for generation of dummy data
@@ -35,8 +36,30 @@ class local_entities_generator extends testing_module_generator {
     public function create_entities(array $data): int {
 
         $data = (object)(array) $data;
-        $sm = new settings_manager();
-        $id = $sm->update_or_createentity($data);
-        return $id;
+        // Force required default values for the entity.
+        $data->addresscount = $data->addresscount ?? 0;
+        $data->entitytype = $data->entitytype ?? 'location';
+        $data->description = $data->description ?? '';
+        $data->pricefactor = $data->pricefactor ?? 1.0;
+        $data->cfitemid = $data->cfitemid ?? 0;
+        $data->daysofweek = json_decode($data->daysofweek ?? "") ?? [];
+        $data->starthours = json_decode($data->starthours ?? "") ?? [];
+        $data->startminutes = json_decode($data->startminutes ?? "") ?? [];
+        $data->endhours = json_decode($data->endhours ?? "") ?? [];
+        $data->endminutes = json_decode($data->endminutes ?? "") ?? [];
+
+        $ajaxargs = json_decode(json_encode($data), true);
+
+        //$sm = new settings_manager();
+        //$id = $sm->update_or_createentity($data);
+        //return $id;
+        // Simulate ajax submission to obtain correct defaults and session key.
+        $submitdata = edit_dynamic_form::mock_ajax_submit($ajaxargs);
+        // Actuall creation and processing form.
+        $mform = new edit_dynamic_form(null, null, 'post', '', [], true, $submitdata, true);
+        $mform->set_data_for_dynamic_submission();
+        //$this->assertTrue($mform->is_validated());
+        $res = $mform->process_dynamic_submission();
+        return ($res->id ?? -1);
     }
 }
